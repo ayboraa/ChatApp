f2f596ee667997e0418a2f2b0591ae6da6d888f9
aybora <asokanta0@gmail.com>
2023-05-26 01:19:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Server
+{
+    public class ServerMain
+    {
+
+
+        public static async Task Main(string[] args)
+        {
+
+            Server mainServer = new Server();
+            await mainServer.StartListening();
+
+        }
+
+
+    }
+}
